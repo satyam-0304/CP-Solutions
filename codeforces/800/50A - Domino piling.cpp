d20139f04eabb16b9aa99c80a3a8cdd645a51dda
Satyam Kandpal <kandpalsatyam30@gmail.com>
2026-09-19 16:46:23 +0530

    Add Domino piling (800)

---
+++ codeforces/800/50A - Domino piling.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// --- Speed Optimization ---
+void fast_io() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+}
+
+// --- Type Aliases & Utilities ---
+using ll = long long;
+using ld = long double;
+const ll INF = 1e18;
+const ll MOD = 1e9 + 7; // 1000000007
+
+// --- Core Logic Goes Here ---
+void solve() {
+
+    // Your code starts here
+    int n,m;
+    cin >> n >> m;
+
+    int x = n*m;
+    cout << (x/2) << '\n';
+    
+    
+}
+
+int main() {
+    // Optimize standard I/O operations
+    fast_io();
+
+    
+    solve();
+
+    return 0;
+}
